2c582184645f89914c53a0021c23412bf83211e6
avenging@hotmail.com <avenging@hotmail.com>
2017-10-11 21:51:28 +0000

    Fix missing import

--- src/main/java/com/redhat/coolstore/rest/DevOpsEndpoint.java
+++ src/main/java/com/redhat/coolstore/rest/DevOpsEndpoint.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.stereotype.Component;
 
 @Path("/")
 @Component
